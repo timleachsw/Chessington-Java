7d4ed9bdbfe37518ed4852c541fd1472fb8bec02
Tim Leach <Tim.Leach@softwire.com>
2019-11-28 11:59:29 +0000

    King moves properly from centre

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -6,6 +6,7 @@ import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class King extends AbstractPiece {
@@ -15,6 +16,17 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowedMoves = new LinkedList<>();
+
+        // loop through each surrounding square
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (!(i == 0 && j == 0)) {  // can't move to itself!
+                    allowedMoves.add(new Move(from, new Coordinates(from.getRow() + i, from.getCol() + j)));
+                }
+            }
+        }
+
+        return allowedMoves;
     }
 }
--- src/test/java/training/chessington/model/pieces/KingTest.java
+++ src/test/java/training/chessington/model/pieces/KingTest.java
@@ -1,7 +1,40 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class KingTest {
+    @Test
+    public void KingMovesProperlyFromCentre() {
+        // arrange
+        Board board = Board.empty();
+        Piece king = new King(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(4, 4);
+        Move[] allowedMoves = {
+                new Move(coords, new Coordinates(3, 3)),
+                new Move(coords, new Coordinates(3, 4)),
+                new Move(coords, new Coordinates(3, 5)),
+                new Move(coords, new Coordinates(4, 3)),
+                new Move(coords, new Coordinates(4, 5)),
+                new Move(coords, new Coordinates(5, 3)),
+                new Move(coords, new Coordinates(5, 4)),
+                new Move(coords, new Coordinates(5, 5))
+        };
+
+        // act
+        List<Move> moves = king.getAllowedMoves(coords, board);
+
+        // assert
+        assertThat(moves).containsExactlyInAnyOrder(allowedMoves);
+    }
 }
