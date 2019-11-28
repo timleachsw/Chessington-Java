477eb546d413d8654b8d541cd89584108194648c
Tim Leach <Tim.Leach@softwire.com>
2019-11-28 15:21:32 +0000

    Refactored by creating class AbstractRayMover

---
+++ src/main/java/training/chessington/model/pieces/AbstractRayMover.java
@@ -0,0 +1,45 @@
+package training.chessington.model.pieces;
+
+import training.chessington.model.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// a piece that moves in rays i.e. bishop, rook, and queen
+public abstract class AbstractRayMover extends AbstractPiece {
+    protected AbstractRayMover(PieceType type, PlayerColour colour) {
+        super(type, colour);
+    }
+
+    @Override
+    public List<Move> getAllowedMoves(Coordinates from, Board board) {
+        List<Move> allowedMoves = new LinkedList<>();
+
+        // march in each direction until we hit a piece or the edge of the board
+        // if we hit an enemy piece, we can capture, but we must stop there i.e. we can't jump over an enemy piece
+        int oldRow = from.getRow();
+        int oldCol = from.getCol();
+        for (Direction direction: getMoveDirections()) {
+            // generate ray in that direction
+            List<Coordinates> ray = board.raycast(from, direction);
+
+            for (Coordinates potential: ray) {
+                // allowable move if empty or enemy piece
+                if (!board.isOwnPiece(potential, colour)) {
+                    allowedMoves.add(new Move(from, potential));
+                } else {
+                    break;
+                }
+
+                // if it's an enemy piece, however, we have to stop there
+                if (board.isEnemyPiece(potential, colour)) {
+                    break;
+                }
+            }
+        }
+
+        return allowedMoves;
+    }
+
+    protected abstract List<Direction> getMoveDirections();
+}
--- src/main/java/training/chessington/model/pieces/Bishop.java
+++ src/main/java/training/chessington/model/pieces/Bishop.java
@@ -6,39 +6,18 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Bishop extends AbstractPiece {
+public class Bishop extends AbstractRayMover {
     public Bishop(PlayerColour colour) {
         super(PieceType.BISHOP, colour);
     }
 
     @Override
-    public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        List<Move> allowedMoves = new LinkedList<>();
-
-        // march in each direction until we hit a piece or the edge of the board
-        // if we hit an enemy piece, we can capture, but we must stop there i.e. we can't jump over an enemy piece
-        int oldRow = from.getRow();
-        int oldCol = from.getCol();
-        Direction[] diagonals = {Direction.NORTHEAST, Direction.NORTHWEST, Direction.SOUTHWEST, Direction.SOUTHEAST};
-        for (Direction direction: diagonals) {
-            // generate ray in that direction
-            List<Coordinates> ray = board.raycast(from, direction);
-
-            for (Coordinates potential: ray) {
-                // allowable move if empty or enemy piece
-                if (!board.isOwnPiece(potential, colour)) {
-                    allowedMoves.add(new Move(from, potential));
-                } else {
-                    break;
-                }
-
-                // if it's an enemy piece, however, we have to stop there
-                if (board.isEnemyPiece(potential, colour)) {
-                    break;
-                }
-            }
-        }
-
-        return allowedMoves;
+    protected List<Direction> getMoveDirections() {
+        return List.of(
+                Direction.NORTHEAST,
+                Direction.NORTHWEST,
+                Direction.SOUTHWEST,
+                Direction.SOUTHEAST
+        );
     }
 }
