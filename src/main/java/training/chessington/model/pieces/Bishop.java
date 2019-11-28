065736a5adda023a8c94b12f0fd6c31238e71173
Tim Leach <Tim.Leach@softwire.com>
2019-11-28 14:59:56 +0000

    Bishop moves properly

--- src/main/java/training/chessington/model/pieces/Bishop.java
+++ src/main/java/training/chessington/model/pieces/Bishop.java
@@ -1,11 +1,9 @@
 package training.chessington.model.pieces;
 
-import training.chessington.model.Board;
-import training.chessington.model.Coordinates;
-import training.chessington.model.Move;
-import training.chessington.model.PlayerColour;
+import training.chessington.model.*;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Bishop extends AbstractPiece {
@@ -15,6 +13,30 @@ public class Bishop extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowedMoves = new LinkedList<>();
+
+        // march in each direction until we hit a piece or the edge of the board
+        // if we hit an enemy piece, we can capture, but we must stop there i.e. we can't jump over an enemy piece
+        int oldRow = from.getRow();
+        int oldCol = from.getCol();
+        Direction[] diagonals = {Direction.NORTHEAST, Direction.NORTHWEST, Direction.SOUTHWEST, Direction.SOUTHEAST};
+        for (Direction direction: diagonals) {
+            // generate ray in that direction
+            List<Coordinates> ray = board.raycast(from, direction);
+
+            for (Coordinates potential: ray) {
+                // allowable move if empty or enemy piece
+                if (!board.isOwnPiece(potential, colour)) {
+                    allowedMoves.add(new Move(from, potential));
+                }
+
+                // if it's an enemy piece, however, we have to stop there
+                if (board.isEnemyPiece(potential, colour)) {
+                    break;
+                }
+            }
+        }
+
+        return allowedMoves;
     }
 }
