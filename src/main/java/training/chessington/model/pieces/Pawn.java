fea70d7737ae2e28d58c229ea7ca54d73c734464
Tim Leach <Tim.Leach@softwire.com>
2019-11-28 10:54:32 +0000

    Green for red-2

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -18,10 +18,25 @@ public class Pawn extends AbstractPiece {
         ArrayList<Move> allowedMoves = new ArrayList<>();
 
         // white moves up one square, black moves down one square
-        int newRow = from.getRow() + (colour == PlayerColour.BLACK ? 1 : -1);
+        boolean isBlack = colour == PlayerColour.BLACK;
+        int oldRow = from.getRow();
+        int newRow = oldRow + (isBlack ? 1 : -1);
 
         // add move
         allowedMoves.add(new Move(from, new Coordinates(newRow, from.getCol())));
+
+        // if it hasn't moved yet (i.e. is on row 1 for black, 6 for white)
+        // note - doesn't check move history but row, therefore is assuming the board has been setup
+        // for a normal chess game
+
+        if (from.getRow() == (isBlack ? 1 : 6)) {
+            // is on start row therefore hasn't moved yet
+            newRow = oldRow + (isBlack ? 2 : -2);
+
+            // add move
+            allowedMoves.add(new Move(from, new Coordinates(newRow, from.getCol())));
+        }
+
         return allowedMoves;
     }
 }
